fe73afb6675379e4269143365ea051b27891dbfa
Ian <ianrodrigues2001@hotmail.com>
2023-11-21 16:33:01 -0300

    Adicionando exercício 5

---
+++ semana3/aula_21_11_23/src/conversortemperatura/Main.java
@@ -0,0 +1,46 @@
+package conversortemperatura;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        ConversorTemperatura convTemp = new ConversorTemperatura();
+        int op;
+        float _temperatura;
+
+        do {
+            System.out.println("    MENU    ");
+
+            System.out.println("1. Converter Celsius para Fahrenheit");
+            System.out.println("2. Converter Fahrenheit para Celsius");
+            System.out.println("3. Sair");
+
+            op = entrada.nextInt();
+
+            if(op == 1){
+
+                System.out.println("Digite a temperatura em Celsius para ser convertida: ");
+                _temperatura = entrada.nextFloat();
+
+                convTemp.CelsiusFahrenheit(_temperatura);
+
+            } else if (op == 2){
+
+                System.out.println("Digite a temperatura em Fahrenheit para ser convertida: ");
+                _temperatura = entrada.nextFloat();
+
+                convTemp.FahrenheitCelsius(_temperatura);
+
+            } else if (op < 1 || op > 3){
+
+                System.out.println("Opção inexistente!");
+
+            }
+
+        }while(op != 3);
+
+        System.out.println("Até a próxima");
+
+    }
+}
