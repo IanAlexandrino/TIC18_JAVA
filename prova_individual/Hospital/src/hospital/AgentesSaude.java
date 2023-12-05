9342d02439deb8b4b941312834081c9e20c9082d
Ian <ianrodrigues2001@hotmail.com>
2023-12-05 16:20:18 -0300

    Adicionando ProcedimentoCompleto, implementando a classe Procedimento e mudando a classe AgentesSaude

--- prova_individual/Hospital/src/hospital/AgentesSaude.java
+++ prova_individual/Hospital/src/hospital/AgentesSaude.java
@@ -1,11 +1,16 @@
 package hospital;
 
+import java.util.ArrayList;
+
 public class AgentesSaude {
     private Medico medico;
     private Enfermeiro enfermeiro;
 
+    private ArrayList<AgentesSaude> agentesSaude;
+
     public AgentesSaude(){
         medico = new Medico();
         enfermeiro = new Enfermeiro();
+        agentesSaude = new ArrayList<>();
     }
 }
--- prova_individual/Hospital/src/hospital/Procedimento.java
+++ prova_individual/Hospital/src/hospital/Procedimento.java
@@ -1,4 +1,15 @@
 package hospital;
 
+import java.util.ArrayList;
+
 public class Procedimento {
+    private String nome;
+    private String tipoProcedimento;
+    private AgentesSaude agentesSaude;
+    private ArrayList<AgentesSaude> agentesSaudeColecao;
+
+    public Procedimento(){
+        agentesSaude = new AgentesSaude();
+        agentesSaudeColecao = new ArrayList<>();
+    }
 }
