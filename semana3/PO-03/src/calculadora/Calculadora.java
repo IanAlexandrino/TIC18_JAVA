07077d6de32825612bf0f30b2a56bf7a3b478736
Ian Rodrigues Alexandrino <133264089+IanAlexandrino@users.noreply.github.com>
2023-12-16 21:58:53 -0300

    Implementando primeira parte da classe Calculadora

--- semana3/PO-03/src/calculadora/Calculadora.java
+++ semana3/PO-03/src/calculadora/Calculadora.java
@@ -2,6 +2,100 @@ package calculadora;
 
 public class Calculadora {
 
+    private int numInteiro1;
+    private int numInteiro2;
+    private int numFloat1;
+    private int numFloat2;
+    private String resultado;
 
+    public Calculadora(){
+
+    }
+
+    public String operacaoAdicaoInt(int _num1, int _num2){
+
+        this.numInteiro1 = _num1;
+        this.numInteiro2 = _num2;
+
+        resultado = "O resultado da soma de " + numInteiro1 + " e " + numInteiro2 + " é: " + (numInteiro1 + numInteiro2);
+
+        return resultado;
+    }
+
+    public String operacaoAdicaoFloat(int _num1, int _num2){
+
+        this.numFloat1 = _num1;
+        this.numFloat2 = _num2;
+
+        resultado = "O resultado da soma de " + numFloat1 + " e " + numFloat2 + " é: " + (numFloat1 + numFloat2);
+
+        return resultado;
+    }
+
+    public String operacaoSubtracaoInt(int _num1, int _num2){
+
+        this.numInteiro1 = _num1;
+        this.numInteiro2 = _num2;
+
+        resultado = "O resultado da soma de " + numInteiro1 + " e " + numInteiro2 + " é: " + (numInteiro1 - numInteiro2);
+
+        return resultado;
+
+    }
+
+    public String operacaoSubtracaoFloat(int _num1, int _num2){
+
+        this.numFloat1 = _num1;
+        this.numFloat2 = _num2;
+
+        resultado = "O resultado da soma de " + numFloat1 + " e " + numFloat2 + " é: " + (numFloat1 - numFloat2);
+
+        return resultado;
+
+    }
+
+    public String operacaoMultiplicacaoInt(int _num1, int _num2){
+
+        this.numInteiro1 = _num1;
+        this.numInteiro2 = _num2;
+
+        resultado = "O resultado da soma de " + numInteiro1 + " e " + numInteiro2 + " é: " + (numInteiro1 * numInteiro2);
+
+        return resultado;
+
+    }
+
+    public String operacaoMultiplicacaoFloat(int _num1, int _num2){
+
+        this.numFloat1 = _num1;
+        this.numFloat2 = _num2;
+
+        resultado = "O resultado da soma de " + numFloat1 + " e " + numFloat2 + " é: " + (numFloat1 * numFloat2);
+
+        return resultado;
+
+    }
+
+    public String operacaoDivisaoInt(int _num1, int _num2){
+
+        this.numInteiro1 = _num1;
+        this.numInteiro2 = _num2;
+
+        resultado = "O resultado da soma de " + numInteiro1 + " e " + numInteiro2 + " é: " + (numInteiro1 / numInteiro2);
+
+        return resultado;
+
+    }
+
+    public String operacaoDivisaoFloat(int _num1, int _num2){
+
+        this.numFloat1 = _num1;
+        this.numFloat2 = _num2;
+
+        resultado = "O resultado da soma de " + numFloat1 + " e " + numFloat2 + " é: " + (numFloat1 / numFloat2);
+
+        return resultado;
+
+    }
 
 }
--- semana3/PO-03/src/calculadora/Main.java
+++ semana3/PO-03/src/calculadora/Main.java
@@ -1,7 +1,20 @@
 package calculadora;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner entrada = new Scanner(System.in);
+        Calculadora calculadora = new Calculadora();
+        float auxNum1;
+        float auxNum2;
+
+        System.out.println(calculadora.operacaoAdicaoInt(1, 2));
+
+        System.out.println(num);
+        System.out.println(num2);
+
     }
 }
