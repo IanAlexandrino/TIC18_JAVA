aaf29bad5004beead67e0ad141ce8112c1c8403f
Ian Rodrigues Alexandrino <133264089+IanAlexandrino@users.noreply.github.com>
2023-12-16 22:25:33 -0300

    Implementando Main da Calculadora

--- semana3/PO-03/src/calculadora/Calculadora.java
+++ semana3/PO-03/src/calculadora/Calculadora.java
@@ -4,8 +4,8 @@ public class Calculadora {
 
     private int numInteiro1;
     private int numInteiro2;
-    private int numFloat1;
-    private int numFloat2;
+    private float numFloat1;
+    private float numFloat2;
     private String resultado;
 
     public Calculadora(){
@@ -17,17 +17,17 @@ public class Calculadora {
         this.numInteiro1 = _num1;
         this.numInteiro2 = _num2;
 
-        resultado = "O resultado da soma de " + numInteiro1 + " e " + numInteiro2 + " é: " + (numInteiro1 + numInteiro2);
+        resultado = "\nO resultado da soma de " + numInteiro1 + " e " + numInteiro2 + " é: " + (numInteiro1 + numInteiro2) + "\n";
 
         return resultado;
     }
 
-    public String operacaoAdicaoFloat(int _num1, int _num2){
+    public String operacaoAdicaoFloat(float _num1, float _num2){
 
         this.numFloat1 = _num1;
         this.numFloat2 = _num2;
 
-        resultado = "O resultado da soma de " + numFloat1 + " e " + numFloat2 + " é: " + (numFloat1 + numFloat2);
+        resultado = "\nO resultado da soma de " + numFloat1 + " e " + numFloat2 + " é: " + (numFloat1 + numFloat2) + "\n";
 
         return resultado;
     }
@@ -37,18 +37,18 @@ public class Calculadora {
         this.numInteiro1 = _num1;
         this.numInteiro2 = _num2;
 
-        resultado = "O resultado da soma de " + numInteiro1 + " e " + numInteiro2 + " é: " + (numInteiro1 - numInteiro2);
+        resultado = "\nO resultado da soma de " + numInteiro1 + " e " + numInteiro2 + " é: " + (numInteiro1 - numInteiro2) + "\n";
 
         return resultado;
 
     }
 
-    public String operacaoSubtracaoFloat(int _num1, int _num2){
+    public String operacaoSubtracaoFloat(float _num1, float _num2){
 
         this.numFloat1 = _num1;
         this.numFloat2 = _num2;
 
-        resultado = "O resultado da soma de " + numFloat1 + " e " + numFloat2 + " é: " + (numFloat1 - numFloat2);
+        resultado = "\nO resultado da soma de " + numFloat1 + " e " + numFloat2 + " é: " + (numFloat1 - numFloat2) + "\n";
 
         return resultado;
 
@@ -59,18 +59,18 @@ public class Calculadora {
         this.numInteiro1 = _num1;
         this.numInteiro2 = _num2;
 
-        resultado = "O resultado da soma de " + numInteiro1 + " e " + numInteiro2 + " é: " + (numInteiro1 * numInteiro2);
+        resultado = "\nO resultado da soma de " + numInteiro1 + " e " + numInteiro2 + " é: " + (numInteiro1 * numInteiro2) + "\n";
 
         return resultado;
 
     }
 
-    public String operacaoMultiplicacaoFloat(int _num1, int _num2){
+    public String operacaoMultiplicacaoFloat(float _num1, float _num2){
 
         this.numFloat1 = _num1;
         this.numFloat2 = _num2;
 
-        resultado = "O resultado da soma de " + numFloat1 + " e " + numFloat2 + " é: " + (numFloat1 * numFloat2);
+        resultado = "\nO resultado da soma de " + numFloat1 + " e " + numFloat2 + " é: " + (numFloat1 * numFloat2) + "\n";
 
         return resultado;
 
@@ -81,18 +81,18 @@ public class Calculadora {
         this.numInteiro1 = _num1;
         this.numInteiro2 = _num2;
 
-        resultado = "O resultado da soma de " + numInteiro1 + " e " + numInteiro2 + " é: " + (numInteiro1 / numInteiro2);
+        resultado = "\nO resultado da soma de " + numInteiro1 + " e " + numInteiro2 + " é: " + (numInteiro1 / numInteiro2) + "\n";
 
         return resultado;
 
     }
 
-    public String operacaoDivisaoFloat(int _num1, int _num2){
+    public String operacaoDivisaoFloat(float _num1, float _num2){
 
         this.numFloat1 = _num1;
         this.numFloat2 = _num2;
 
-        resultado = "O resultado da soma de " + numFloat1 + " e " + numFloat2 + " é: " + (numFloat1 / numFloat2);
+        resultado = "\nO resultado da soma de " + numFloat1 + " e " + numFloat2 + " é: " + (numFloat1 / numFloat2) + "\n";
 
         return resultado;
 
