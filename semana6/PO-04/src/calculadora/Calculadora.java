6fb4448661b950b873433b8d884662b2b58471fc
Ian Rodrigues Alexandrino <133264089+IanAlexandrino@users.noreply.github.com>
2023-12-22 11:13:46 -0300

    Terminando primeira parte da PO-04

--- semana6/PO-04/src/calculadora/Calculadora.java
+++ semana6/PO-04/src/calculadora/Calculadora.java
@@ -1,8 +1,16 @@
 package calculadora;
 
+import java.util.ArrayList;
+
 public class Calculadora {
 
-    public static int soma(int num1, int num2){
+    private ArrayList<Float> listaOperacao = new ArrayList<>();
+
+    public Calculadora(){
+
+    }
+
+    public int soma(int num1, int num2){
 
         int soma;
         soma = num1 + num2;
@@ -10,7 +18,7 @@ public class Calculadora {
         return soma;
     }
 
-    public static float soma(float num1, float num2){
+    public float soma(float num1, float num2){
 
         float soma;
         soma = num1 + num2;
@@ -18,7 +26,7 @@ public class Calculadora {
         return soma;
     }
 
-    public static int subtracao(int num1, int num2){
+    public int subtracao(int num1, int num2){
 
         int subtracao;
         subtracao = num1 - num2;
@@ -26,7 +34,7 @@ public class Calculadora {
         return subtracao;
     }
 
-    public static float subtracao(float num1, float num2){
+    public float subtracao(float num1, float num2){
 
         float subtracao;
         subtracao = num1 - num2;
@@ -34,7 +42,7 @@ public class Calculadora {
         return subtracao;
     }
 
-    public static int multiplicacao(int num1, int num2){
+    public int multiplicacao(int num1, int num2){
 
         int multiplicacao;
         multiplicacao = num1 * num2;
@@ -42,7 +50,7 @@ public class Calculadora {
         return multiplicacao;
     }
 
-    public static float multiplicacao(float num1, float num2){
+    public float multiplicacao(float num1, float num2){
 
         float multiplicacao;
         multiplicacao = num1 * num2;
@@ -50,7 +58,7 @@ public class Calculadora {
         return multiplicacao;
     }
 
-    public static int divisao(int num1, int num2){
+    public int divisao(int num1, int num2){
 
         int divisao;
         divisao = num1 / num2;
@@ -58,7 +66,7 @@ public class Calculadora {
         return divisao;
     }
 
-    public static float divisao(float num1, float num2){
+    public float divisao(float num1, float num2){
 
         float divisao;
         divisao = num1 / num2;
@@ -66,4 +74,102 @@ public class Calculadora {
         return divisao;
     }
 
+    public void adicionaNumerosLista(float num1){
+        listaOperacao.add(num1);
+    }
+
+    public void retornaOperacao(int op){
+
+        float operacao = 0;
+
+        switch (op){
+            case 1:
+
+                for (Float n : listaOperacao){
+                    operacao += n.floatValue();
+                }
+
+                if ( operacao == (int) operacao){
+
+                    System.out.println("O resultado da lista de soma é: " + (int) operacao + "\n");
+
+                } else {
+
+                    System.out.println("O resultado da lista de soma é: " + operacao + "\n");
+
+                }
+
+                listaOperacao.clear();
+
+                break;
+
+            case 2:
+
+                operacao = listaOperacao.get(0);
+
+                for (int n = 1 ; n < listaOperacao.size() ; n++){
+                    operacao -= listaOperacao.get(n);
+                }
+
+                if ( operacao == (int) operacao){
+
+                    System.out.println("O resultado da lista de subtração é: " + (int) operacao + "\n");
+
+                } else {
+
+                    System.out.println("O resultado da lista de subtração é: " + operacao + "\n");
+
+                }
+
+                listaOperacao.clear();
+
+                break;
+
+            case 3:
+
+                operacao = listaOperacao.get(0);
+
+                for (int n = 1 ; n < listaOperacao.size() ; n++){
+                    operacao *= listaOperacao.get(n);
+                }
+
+                if ( operacao == (int) operacao){
+
+                    System.out.println("O resultado da lista de multiplicação é: " + (int) operacao + "\n");
+
+                } else {
+
+                    System.out.println("O resultado da lista de multiplicação é: " + operacao + "\n");
+
+                }
+
+                listaOperacao.clear();
+
+                break;
+
+            case 4:
+
+                operacao = listaOperacao.get(0);
+
+                for (int n = 1 ; n < listaOperacao.size() ; n++){
+                    operacao /= listaOperacao.get(n);
+                }
+
+                if ( operacao == (int) operacao){
+
+                    System.out.println("O resultado da lista de divisão é: " + (int) operacao + "\n");
+
+                } else {
+
+                    System.out.println("O resultado da lista de divisão é: " + operacao + "\n");
+
+                }
+
+                listaOperacao.clear();
+
+                break;
+        }
+
+
+    }
 }
