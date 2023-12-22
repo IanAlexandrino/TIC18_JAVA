4f08f9e23ec005ded33e134f52ab157314b9f7c7
Ian Rodrigues Alexandrino <133264089+IanAlexandrino@users.noreply.github.com>
2023-12-22 11:30:03 -0300

    Adicionando classe ItemPedido

---
+++ semana6/PO-04/src/gerenciamentopedidos/ItemPedido.java
@@ -0,0 +1,24 @@
+package gerenciamentopedidos;
+
+public class ItemPedido {
+
+    private String nomeItem;
+    private float valorItem;
+
+    public void setNomeItem(String nomeItem) {
+        this.nomeItem = nomeItem;
+    }
+
+    public String getNomeItem() {
+        return nomeItem;
+    }
+
+    public void setValorItem(float valorItem) {
+        this.valorItem = valorItem;
+    }
+
+    public float getValorItem() {
+        return valorItem;
+    }
+
+}
--- semana6/PO-04/src/gerenciamentopedidos/Pedido.java
+++ semana6/PO-04/src/gerenciamentopedidos/Pedido.java
@@ -1,6 +1,19 @@
 package gerenciamentopedidos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pedido {
+
+    private int nmrPedido;
+    private String cpfCliente;
+    private ArrayList<ItemPedido> itensPedido;
+    private ItemPedido itemPedido;
+
+    public Pedido(){
+        itensPedido = new ArrayList<>();
+        itemPedido = new ItemPedido();
+    }
 
 
 
