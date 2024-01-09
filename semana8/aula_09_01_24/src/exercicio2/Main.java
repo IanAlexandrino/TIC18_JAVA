05025b394fb405e5a64a171c23dae19a922cb279
Ian <ianrodrigues2001@hotmail.com>
2024-01-09 17:39:16 -0300

    Adicionando 80% do exercício 2 feito

--- semana8/aula_09_01_24/src/exercicio2/Main.java
+++ semana8/aula_09_01_24/src/exercicio2/Main.java
@@ -1,4 +1,49 @@
 package exercicio2;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        Usuarios usuarios = new Usuarios();
+        boolean menu = true;
+        int opSwitch;
+
+        while(menu){
+
+            System.out.println("1 - insere");
+            System.out.println("2 - busca");
+            System.out.println("3 - lista todos");
+            System.out.println("4 - exclui");
+
+            opSwitch = entrada.nextInt();
+
+            switch (opSwitch){
+                case 1:
+                    usuarios.inserirUsuario();
+                    break;
+
+                case 2:
+                    usuarios.buscarUsuario();
+                    break;
+
+                case 3:
+                    usuarios.listarTodosUsuarios();
+                    break;
+
+                case 4:
+                    usuarios.excluirUsuario();
+                    break;
+
+                default:
+                    System.out.println("Opção errada");
+                    menu = false;
+            }
+
+        }
+
+    }
+
 }
--- semana8/aula_09_01_24/src/exercicio2/Usuarios.java
+++ semana8/aula_09_01_24/src/exercicio2/Usuarios.java
@@ -1,4 +1,102 @@
 package exercicio2;
 
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Usuarios {
+
+    private ArrayList<Usuario> usuarios;
+
+    public Usuarios(){
+
+        usuarios = new ArrayList();
+
+    }
+
+    public void inserirUsuario(){
+
+        Scanner entrada = new Scanner(System.in);
+        Usuario usuario = new Usuario();
+
+        String auxNome;
+        String auxEmail;
+        String auxSenha;
+
+        System.out.println("Insira o nome de usuário: ");
+        auxNome = entrada.nextLine();
+
+        System.out.println("Insira o email do usuário: ");
+        auxEmail = entrada.nextLine();
+
+        System.out.println("Insira a senha do usuário: ");
+        auxSenha = entrada.nextLine();
+
+        usuario.setNome(auxNome);
+        usuario.setEmail(auxEmail);
+        usuario.setSenha(auxSenha);
+
+        usuarios.add(usuario);
+    }
+
+    public void buscarUsuario(){
+
+        Scanner entrada = new Scanner(System.in);
+        String auxNomeBusca;
+
+        System.out.println("Digite o usuário que você quer buscar: ");
+        auxNomeBusca = entrada.nextLine();
+
+        for (Usuario usuario : usuarios){
+
+            if (Objects.equals(usuario.getNome(), auxNomeBusca)){
+
+                System.out.println("Usuário: \n");
+                System.out.println("Nome: " + usuario.getNome());
+                System.out.println("Email: " + usuario.getEmail() + "\n");
+
+            } else {
+
+                System.out.println("Usuário não encontrado!");
+
+            }
+        }
+
+    }
+
+    public void listarTodosUsuarios(){
+
+        int i = 1;
+
+        for (Usuario usuario : usuarios){
+            System.out.println(i + "º Usuário\n");
+            System.out.println("Nome: " + usuario.getNome());
+            System.out.println("Email: " + usuario.getEmail() + "\n");
+            i++;
+        }
+
+    }
+
+    public void excluirUsuario(){
+
+        Scanner entrada = new Scanner(System.in);
+        String auxNomeExcluir;
+
+        System.out.println("Digite o usuário que você quer excluir: ");
+        auxNomeExcluir = entrada.nextLine();
+
+        for (Usuario usuario : usuarios){
+
+            if (Objects.equals(usuario.getNome(), auxNomeExcluir)){
+
+                usuarios.remove(usuario);
+
+            } else {
+
+                System.out.println("Usuário não encontrado!");
+
+            }
+        }
+
+    }
 }
