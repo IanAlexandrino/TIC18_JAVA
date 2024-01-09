93da112a0b1d654ef3dce9c68d009398640b8a79
Ian <ianrodrigues2001@hotmail.com>
2024-01-09 16:35:45 -0300

    Terminando primeiro exercício

--- semana8/aula_09_01_24/src/exercicio1/CriarArquivo.java
+++ semana8/aula_09_01_24/src/exercicio1/CriarArquivo.java
@@ -1,4 +1,62 @@
 package exercicio1;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class CriarArquivo {
+
+    private String nomeArquivo;
+    private String textoArquivo;
+
+    public CriarArquivo (){
+
+    }
+
+    public void criandoArquivo() {
+
+        boolean op = true;
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Olá, digite o nome do arquivo que você quer criar: ");
+
+        nomeArquivo = entrada.nextLine();
+
+        File arquivo = new File("C:/Users/06569235559/Documents/TIC18_JAVA/semana8/aula_09_01_24/src/exercicio1/" + nomeArquivo + ".txt");
+
+        try {
+            arquivo.createNewFile();
+            System.out.print("Arquivo criado com sucesso!");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+            try (FileWriter fw = new FileWriter(arquivo);
+                 BufferedWriter br = new BufferedWriter(fw)) {
+
+                while(op) {
+
+                    System.out.println("Digite uma linha para por no arquivo: ");
+                    textoArquivo = entrada.nextLine();
+
+                    if (textoArquivo == "") {
+                        op = false;
+                        return;
+                    }
+
+                    br.write(textoArquivo);
+                    br.newLine();
+
+                }
+
+            } catch (IOException e){
+
+                e.printStackTrace();
+
+            }
+
+    }
 }
--- semana8/aula_09_01_24/src/exercicio1/Main.java
+++ semana8/aula_09_01_24/src/exercicio1/Main.java
@@ -2,7 +2,9 @@ package exercicio1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        CriarArquivo criaArquivo = new CriarArquivo();
+
+        criaArquivo.criandoArquivo();
 
     }
 }
