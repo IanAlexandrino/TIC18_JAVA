b31ccfcd18b6e4ea0a9efa07c3796210c818a576
Ian <ianrodrigues2001@hotmail.com>
2024-01-12 11:52:40 -0300

    Atuazlizações na PO-04 e na classe Checkpoint da PO-05

---
+++ semana7/PO-05/src/empresaonibus/Checkpoint.java
@@ -0,0 +1,16 @@
+package empresaonibus;
+
+public class Checkpoint {
+
+    private final String checkpoint;
+
+    public Checkpoint(String checkpoint){
+
+        this.checkpoint = checkpoint;
+
+    }
+
+    public String getCheckpoint() {
+        return checkpoint;
+    }
+}
