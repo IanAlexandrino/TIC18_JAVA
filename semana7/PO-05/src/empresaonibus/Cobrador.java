86ebf195e949ae5df0d0ed18a003bde66d2e6155
Ian <ianrodrigues2001@hotmail.com>
2024-01-12 11:05:55 -0300

    Implementando classe Jornada e adicionando nas classes Motorista e Cobrador

--- semana7/PO-05/src/empresaonibus/Cobrador.java
+++ semana7/PO-05/src/empresaonibus/Cobrador.java
@@ -1,4 +1,18 @@
 package empresaonibus;
 
 public class Cobrador {
+
+    private Jornada jornada;
+
+    public Cobrador(){
+
+    }
+
+    public void setJornada(int intervaloTempo) {
+        this.jornada = new Jornada(intervaloTempo);
+    }
+
+    public void getJornada() {
+        System.out.println(jornada.getIntervaloTempo());
+    }
 }
--- semana7/PO-05/src/empresaonibus/Motorista.java
+++ semana7/PO-05/src/empresaonibus/Motorista.java
@@ -1,4 +1,18 @@
 package empresaonibus;
 
 public class Motorista {
+
+    private Jornada jornada;
+
+    public Motorista(){
+
+    }
+
+    public void setJornada(int intervaloTempo) {
+        this.jornada = new Jornada(intervaloTempo);
+    }
+
+    public void getJornada() {
+        System.out.println(jornada.getIntervaloTempo());
+    }
 }
