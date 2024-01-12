f3698ee91ac8f19b61f3eaefcb09e7044affb62e
Ian <ianrodrigues2001@hotmail.com>
2024-01-12 09:37:49 -0300

    Implementando PontosParada

--- semana7/PO-05/src/empresaonibus/PontosParada.java
+++ semana7/PO-05/src/empresaonibus/PontosParada.java
@@ -1,4 +1,18 @@
 package empresaonibus;
 
 public class PontosParada {
+
+    private String local;
+
+    public PontosParada(){
+
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public String getLocal() {
+        return local;
+    }
 }
