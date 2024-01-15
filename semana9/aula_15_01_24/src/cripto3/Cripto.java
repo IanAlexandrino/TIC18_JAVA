359263b0f17a6bc76103c419ea158a12c6b5ad6c
Ian <ianrodrigues2001@hotmail.com>
2024-01-15 11:58:43 -0300

    Adicionando exercício3 completo

--- semana9/aula_15_01_24/src/cripto3/Cripto.java
+++ semana9/aula_15_01_24/src/cripto3/Cripto.java
@@ -1,4 +1,66 @@
 package cripto3;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Cripto {
+    public static void CopiaImagem(String file1, String file2, String senha){
+
+
+
+        try {
+            FileInputStream fileBase = new FileInputStream(file1);
+            boolean eof = false;
+            ArrayList<Integer> data = new ArrayList<>();
+            ArrayList<Integer> dataFinal = new ArrayList<>();
+            int[] byteSenha = {49, 50, 51, 52, 53};
+
+            while (!eof){
+                int input = fileBase.read();
+                if (input != -1){
+
+                    data.add(input);
+
+                } else eof = true;
+
+            }
+
+            int auxByteSenha = 0;
+
+            for (int i = 0; i < data.size(); i++) {
+
+                if (auxByteSenha > 4){
+                    auxByteSenha = 0;
+                    int xorResult = data.get(i) ^ byteSenha[auxByteSenha];
+                    dataFinal.add(xorResult);
+                    auxByteSenha++;
+
+                } else {
+
+                    int xorResult = data.get(i) ^ byteSenha[auxByteSenha];
+                    dataFinal.add(xorResult);
+                    auxByteSenha++;
+
+                }
+
+            }
+
+            FileOutputStream fileDestino = new FileOutputStream(file2);
+
+            for (int i = 0 ; i < data.size() ; i++){
+
+                fileDestino.write(dataFinal.get(i));
+
+            }
+
+            fileBase.close();
+            fileDestino.close();
+
+        } catch (IOException e) {
+            System.out.println("Error --" + e.toString());
+        }
+
+    }
 }
--- semana9/aula_15_01_24/src/cripto3/Main.java
+++ semana9/aula_15_01_24/src/cripto3/Main.java
@@ -1,4 +1,12 @@
 package cripto3;
 
 public class Main {
+    public static void main(String[] args) {
+        String name1 = args[0];
+        String name2 = args[1];
+        String name3 = args[2];
+
+        Cripto.CopiaImagem(name1, name2, name3);
+    }
 }
+
