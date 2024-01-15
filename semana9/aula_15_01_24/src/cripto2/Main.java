dff78fe953fa36561875561a33997076bfc7683e
Ian <ianrodrigues2001@hotmail.com>
2024-01-15 11:13:32 -0300

    Adicionando arquivos base da segunda parte do exercício

---
+++ semana9/aula_15_01_24/src/cripto2/Main.java
@@ -0,0 +1,11 @@
+package cripto2;
+
+import cripto.Cripto;
+
+public class Main {
+    public static void main(String[] args) {
+        String name1 = args[0];
+        String name2 = args[1];
+        Cripto.CopiaImagem(name1, name2);
+    }
+}
