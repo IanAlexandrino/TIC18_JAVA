cabd0866d8e245abecbb93bab420997cc8e876f6
Ian <ianrodrigues2001@hotmail.com>
2024-01-15 11:09:26 -0300

    Adicionando código para copiar imagem completo

--- semana9/aula_15_01_24/src/cripto/Cripto.java
+++ semana9/aula_15_01_24/src/cripto/Cripto.java
@@ -1,4 +1,46 @@
 package cripto;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Cripto {
+
+    public static void CopiaImagem(String file1, String file2){
+
+
+
+        try {
+            FileInputStream fileBase = new FileInputStream(file1);
+            boolean eof = false;
+            ArrayList<Integer> data = new ArrayList<>();
+            while (!eof){
+                int input = fileBase.read();
+                if (input != -1){
+
+                    data.add(input);
+
+                } else eof = true;
+
+            }
+
+            fileBase.close();
+
+            FileOutputStream fileDestino = new FileOutputStream(file2);
+
+            for (int i = 0 ; i < data.size() ; i++){
+
+                fileDestino.write(data.get(i));
+
+            }
+
+            fileDestino.close();
+
+        } catch (IOException e) {
+            System.out.println("Error --" + e.toString());
+        }
+
+    }
+
 }
--- semana9/aula_15_01_24/src/cripto/Main.java
+++ semana9/aula_15_01_24/src/cripto/Main.java
@@ -2,6 +2,8 @@ package cripto;
 
 public class Main {
     public static void main(String[] args) {
-
+        String name1 = args[0];
+        String name2 = args[1];
+        Cripto.CopiaImagem(name1, name2);
     }
 }
