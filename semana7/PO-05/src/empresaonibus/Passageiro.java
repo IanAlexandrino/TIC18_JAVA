6960bb9eeea0b51f435c10b9bd7fb4bb8f4f9029
Ian Rodrigues Alexandrino <133264089+IanAlexandrino@users.noreply.github.com>
2024-01-17 11:04:38 -0300

    Implementando classe Passageiro

--- semana7/PO-05/src/empresaonibus/Passageiro.java
+++ semana7/PO-05/src/empresaonibus/Passageiro.java
@@ -1,4 +1,40 @@
 package empresaonibus;
 
 public class Passageiro {
+
+    private String Nome;
+    private String cartaoPassageiro;
+    private Trecho trechoPassageiro;
+
+    public Passageiro(){
+
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setCartaoPassageiro(String cartaoPassageiro) {
+        this.cartaoPassageiro = cartaoPassageiro;
+    }
+
+    public String getCartaoPassageiro() {
+        return cartaoPassageiro;
+    }
+
+    public void setTrechoPassageiro(Trecho trecho) {
+        this.trechoPassageiro = trecho;
+    }
+
+    public void getTrechoPassageiro() {
+        System.out.println("Local de embarque do passageiro: ");
+        trechoPassageiro.getOrigemTrecho();
+
+        System.out.println("Local de destino do passageiro: ");
+        trechoPassageiro.getDestinoTrecho();
+    }
 }
