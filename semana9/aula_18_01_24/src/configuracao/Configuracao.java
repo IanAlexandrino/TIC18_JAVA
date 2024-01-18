932fa1186bf26526260c9dce7c38aef35bff3cfa
Ian <ianrodrigues2001@hotmail.com>
2024-01-18 16:49:35 -0300

    Bug de arquivos resolvido

---
+++ semana9/aula_18_01_24/src/configuracao/Configuracao.java
@@ -0,0 +1,14 @@
+package configuracao;
+
+public class Configuracao {
+
+    private String alfabeto;
+
+    public void setAlfabeto(String alfabeto) {
+        this.alfabeto = alfabeto;
+    }
+
+    public String getAlfabeto() {
+        return alfabeto;
+    }
+}
---
+++ semana9/aula_18_01_24/src/configuracao/Teste.java
@@ -0,0 +1,42 @@
+package configuracao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Teste {
+
+    @Test
+    void testSetAlfabeto() {
+        Configuracao configuracao = new Configuracao();
+        String alfabeto = "ABCDEFGH";
+
+        try{
+
+            configuracao.setAlfabeto(alfabeto);
+
+        } catch (Exception e1){
+
+            fail("Gerou exceção indevida");
+            e1.printStackTrace();
+
+        }
+
+        assertEquals(alfabeto, configuracao.getAlfabeto());
+
+        alfabeto = null;
+        try{
+
+            configuracao.setAlfabeto(alfabeto);
+
+        } catch (Exception e){
+
+            assertEquals("O alfabeto deve possuir mais de 1 caracter", e.getMessage());
+
+        }
+
+        assertFalse(configuracao.getAlfabeto()==null);
+    }
+
+}
