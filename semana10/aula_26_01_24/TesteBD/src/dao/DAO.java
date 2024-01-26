9d60067ad3294a11ab5d12f4f54c3130c04a4ac4
Ian <ianrodrigues2001@hotmail.com>
2024-01-26 11:36:48 -0300

    Configurando classe para conexão com banco

--- semana10/aula_26_01_24/TesteBD/src/dao/DAO.java
+++ semana10/aula_26_01_24/TesteBD/src/dao/DAO.java
@@ -1,4 +1,37 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DAO {
+
+    private static final String URL = "jdbc:mysql://ul9sz1l10xtxy20l:Gwgs4nNfsdFwQzCxiihh@bvnp9l2wptkemmjpjzu6-mysql.services.clever-cloud.com:3306/bvnp9l2wptkemmjpjzu6";
+    private static final String USER = "ul9sz1l10xtxy20l";
+    private static final String PASSWD = "Gwgs4nNfsdFwQzCxiihh";
+
+    public static Connection conectar(){
+
+        Connection con = null;
+        try{
+
+            con = DriverManager.getConnection(URL, USER, PASSWD);
+            System.out.println("Conexâo feita com sucesso!");
+
+        } catch (SQLException e) {
+
+            System.out.println("Erro na conexâo");
+
+        }
+
+        return con;
+
+    }
+
+    public static void main(String[] args) {
+
+        DAO.conectar();
+
+    }
+
 }
