cab9c475b939c78f54907ffd979f291b2020ca0b
Ian <ianrodrigues2001@hotmail.com>
2024-01-29 12:17:07 -0300

    Adicionando atributo postagens para classe usuario e seus respectivos métodos

--- semana11/aula_20_01_24/RedeSocialBD/src/usuario/Usuario.java
+++ semana11/aula_20_01_24/RedeSocialBD/src/usuario/Usuario.java
@@ -1,10 +1,33 @@
 package usuario;
 
+import java.util.ArrayList;
+
 public class Usuario {
 
     private String login, senha, email;
+    private ArrayList<String> postagens;
 
     public Usuario(){
+
+        postagens = new ArrayList<>();
+
+    }
+
+    public void getPostagens() {
+        for (String postagem : postagens){
+
+            System.out.println(postagem);
+
+        }
+    }
+
+    public void setPostagens(ArrayList<String> _postagens) {
+        postagens.addAll(_postagens);
+    }
+
+    public void addPostagem(String postagem){
+
+        postagens.add(postagem);
 
     }
 
