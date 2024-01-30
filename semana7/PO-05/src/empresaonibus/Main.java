401da01fbedf0089da51114a78e2845dab2f9b23
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-01-29 22:16:07 -0300

    Refatorando lÃógica da classe Trajeto criando uma classe Main e concluindo cadastro de veículos

---
+++ semana7/PO-05/src/empresaonibus/Main.java
@@ -0,0 +1,55 @@
+package empresaonibus;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        EmpresaOnibus empresaOnibus = new EmpresaOnibus();
+        Scanner entrada = new Scanner(System.in);
+        boolean auxwhile = true;
+        int auxMenuPrincipal;
+
+        while (auxwhile){
+
+            System.out.print("##--Empresa Ônibus--##\n\n");
+            System.out.print("|-----------------------------|\n");
+            System.out.print("| Opção 1 - Área de Cadastro  |\n");
+            System.out.print("| Opção 2 - Área de Pesquisa  |\n");
+            System.out.print("| Opção 3 - Simular Viagem    |\n");
+            System.out.print("| Opção 4 - Sair              |\n");
+            System.out.print("|-----------------------------|\n");
+            System.out.print("Digite uma opção: ");
+
+            auxMenuPrincipal = entrada.nextInt();
+
+            switch (auxMenuPrincipal){
+
+                case 1:
+                    empresaOnibus.menuCadastro();
+                    break;
+
+                case 2:
+                    empresaOnibus.menuPesquisa();
+                    break;
+
+                case 3:
+                    empresaOnibus.menuViagem();
+                    break;
+
+                case 4:
+                    auxwhile = false;
+                    System.out.println("Até a próxima!!");
+                    break;
+
+                default:
+                    System.out.println("Opção incorreta, por favor, tente novamente!");
+            }
+
+        }
+
+
+    }
+
+}
