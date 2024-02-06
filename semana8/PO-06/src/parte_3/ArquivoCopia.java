ae2761238bdca74154192e5db24e13ecfb0de1f9
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-02-06 09:50:05 -0300

    Adicionando atributos para a classe ArquivoCopia e método copiaConteudoArquivo

--- semana8/PO-06/src/parte_3/ArquivoCopia.java
+++ semana8/PO-06/src/parte_3/ArquivoCopia.java
@@ -1,4 +1,42 @@
 package parte_3;
 
+import java.io.*;
+
 public class ArquivoCopia {
+
+    private File arquivoOrigem;
+    private File arquivoDestino;
+    private boolean existeOrigem;
+    private boolean existeDestino;
+    private FileReader fr;
+    private BufferedReader br;
+    private FileWriter fw;
+    private BufferedWriter bw;
+
+    public ArquivoCopia(){
+
+        arquivoOrigem = new File("origem.txt");
+        arquivoDestino = new File("destino.txt");
+        existeOrigem = arquivoOrigem.exists();
+        existeDestino = arquivoDestino.exists();
+
+    }
+
+    public void copiaConteudoArquivo() throws IOException {
+
+        if (!existeOrigem){
+
+            arquivoOrigem.createNewFile();
+            arquivoOrigem.mkdir();
+
+        }
+
+        if (!existeDestino){
+
+            arquivoDestino.createNewFile();
+            arquivoDestino.mkdir();
+
+        }
+
+    }
 }
