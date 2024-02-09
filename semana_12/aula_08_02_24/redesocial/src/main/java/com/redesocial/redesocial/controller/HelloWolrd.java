47103fe899b3960dc0fc4fb78e8811ee84486142
Ian <ianrodrigues2001@hotmail.com>
2024-02-09 09:40:16 -0300

    Adicionando classe Usuario e um novo controller que printa usuários

--- semana_12/aula_08_02_24/redesocial/src/main/java/com/redesocial/redesocial/controller/HelloWolrd.java
+++ semana_12/aula_08_02_24/redesocial/src/main/java/com/redesocial/redesocial/controller/HelloWolrd.java
@@ -1,22 +1,34 @@
 package com.redesocial.redesocial.controller;
 
-import org.springframework.stereotype.Controller;
+import com.redesocial.redesocial.modelo.Usuario;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.ArrayList;
+
+@RestController
 public class HelloWolrd {
 
     @RequestMapping("/")
-    @ResponseBody
     public String retornMensagem(){
         return "AAAAAAAAAAAAAAAAAAAAAAA";
     }
 
-    public String retorna(){
+    @RequestMapping("/listausuarios")
+    public ArrayList<Usuario> retornaUsuarios(){
+
+        ArrayList<Usuario> usuarios = new ArrayList<>();
+
+        Usuario u1 = new Usuario(1234567890L, "pedro", "pedro@hotmail.com", "pedropedrao");
+        Usuario u2 = new Usuario(1234467890L, "joao", "joao@hotmail.com", "joaojoaozao");
+        Usuario u3 = new Usuario(1243567890L, "josias", "josias@hotmail.com", "josiasjosiaszao");
+
+        usuarios.add(u1);
+        usuarios.add(u2);
+        usuarios.add(u3);
 
 
-        return
+        return usuarios;
     }
 
 }
