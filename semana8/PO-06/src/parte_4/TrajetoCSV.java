98dfd3eedd0df88aa4f4f0ddb84f37595cf1ce61
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-02-10 04:33:27 -0300

    Commit com alterações inacabadas

--- semana8/PO-06/src/parte_4/TrajetoCSV.java
+++ semana8/PO-06/src/parte_4/TrajetoCSV.java
@@ -1,4 +1,36 @@
 package parte_4;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 public class TrajetoCSV {
+
+    private static final File arquivo = new File("Trajeto.csv");
+    private static final boolean existe = arquivo.exists();
+
+    public static void insereDadosArquivo(Trajeto trajeto) {
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(arquivo, StandardCharsets.ISO_8859_1, existe))){
+
+            if (!existe){
+
+                arquivo.createNewFile();
+                arquivo.mkdir();
+                bw.write("Trajeto\n");
+
+            }
+
+            bw.write(trecho.getOrigemTrecho() + ";" + trecho.getDestinoTrecho() + "\n");
+
+        } catch (IOException e){
+
+            System.out.println("Erro: " + e.getMessage());
+
+        }
+
+    }
+
 }
