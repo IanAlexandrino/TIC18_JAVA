6968bfa531a848908cc781c7148fd97d4f3c2ad1
Ian <ianrodrigues2001@hotmail.com>
2024-02-22 15:54:22 -0300

    Criando classe UsuarioController e UserForm

---
+++ semana_12/aula_08_02_24/redesocial/src/main/java/com/redesocial/redesocial/controller/UsuarioController.java
@@ -0,0 +1,43 @@
+package com.redesocial.redesocial.controller;
+
+import com.redesocial.redesocial.controller.dto.UsuarioDTO;
+import com.redesocial.redesocial.controller.Form.UserForm;
+import com.redesocial.redesocial.modelo.Usuario;
+import com.redesocial.redesocial.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios/")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @PostMapping
+    public UsuarioDTO inserir(@RequestBody UserForm UF){
+
+        Usuario usuario = UF.criaUsuario();
+        usuarioRepository.save(usuario);
+        return new UsuarioDTO(usuario);
+    }
+
+    @GetMapping
+    public List<UsuarioDTO> retornaUsuarios(String name){
+        List<Usuario> listaUsuarios = (ArrayList<Usuario>) usuarioRepository.findAll();
+        List<UsuarioDTO> lista = new ArrayList<UsuarioDTO>();
+        for (Usuario u : listaUsuarios){
+
+            UsuarioDTO ud = new UsuarioDTO(u);
+            lista.add(ud);
+
+        }
+
+
+        return lista;
+    }
+
+}
