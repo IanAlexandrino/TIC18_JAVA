c395c946bfdcd8c1d1b9ca1c7052c27d54491642
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-02-29 17:27:10 -0300

    Implementando classe LanceDTO

--- semana_13/leilao/src/main/java/com/leilao/leilao/controller/dto/LanceDTO.java
+++ semana_13/leilao/src/main/java/com/leilao/leilao/controller/dto/LanceDTO.java
@@ -9,9 +9,31 @@ public class LanceDTO {
 
     private Long Id;
     private Double Valor;
-    private LeilaoDTO Leilao;
-    private ConcorrenteDTO Concorrente;
+    private Leilao Leilao;
+    private Concorrente Concorrente;
 
     public LanceDTO(Lance lance) {
+
+        this.Id = lance.getId();
+        this.Valor = lance.getValor();
+        this.Leilao = lance.getLeilao();
+        this.Concorrente = lance.getConcorrente();
+
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public Double getValor() {
+        return Valor;
+    }
+
+    public com.leilao.leilao.model.Leilao getLeilao() {
+        return Leilao;
+    }
+
+    public com.leilao.leilao.model.Concorrente getConcorrente() {
+        return Concorrente;
     }
 }
