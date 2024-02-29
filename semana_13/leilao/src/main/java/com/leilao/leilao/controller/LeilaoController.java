dd973bd4fc3f8cd7e8aaebb7a47af9ba1a35f46d
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-02-29 19:45:22 -0300

    Adicionando método retornaLeiloes na classe LeilaoController

--- semana_13/leilao/src/main/java/com/leilao/leilao/controller/LeilaoController.java
+++ semana_13/leilao/src/main/java/com/leilao/leilao/controller/LeilaoController.java
@@ -1,4 +1,132 @@
 package com.leilao.leilao.controller;
 
+import com.leilao.leilao.controller.dto.ConcorrenteDTO;
+import com.leilao.leilao.controller.dto.LeilaoDTO;
+import com.leilao.leilao.controller.form.ConcorrenteForm;
+import com.leilao.leilao.model.Concorrente;
+import com.leilao.leilao.model.Leilao;
+import com.leilao.leilao.repository.ConcorrenteRepository;
+import com.leilao.leilao.repository.LeilaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/leilao/")
 public class LeilaoController {
-}
+
+    @Autowired
+    private LeilaoRepository leilaoRepository;
+
+    @GetMapping
+    public List<LeilaoDTO> retornaLeiloes() {
+
+        List<Leilao> listaLeiloes = (ArrayList<Leilao>) leilaoRepository.findAll();
+        List<LeilaoDTO> lista = new ArrayList<LeilaoDTO>();
+        for (Leilao leilao : listaLeiloes){
+
+            LeilaoDTO leilaoDTO = new LeilaoDTO(leilao);
+            lista.add(leilaoDTO);
+
+        }
+        return lista;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> listaLeiloesId(@PathVariable Integer id){
+
+        if (id == null){
+
+            return ResponseEntity.badRequest().build();
+
+        }
+
+        try{
+
+            Concorrente concorrente = concorrenteRepository.getReferenceById(id);
+            ConcorrenteDTO concorrenteDTO = new ConcorrenteDTO(concorrente);
+            return ResponseEntity.ok(concorrenteDTO);
+
+        } catch (Exception e){
+
+            return ResponseEntity.notFound().build();
+
+        }
+
+    }
+
+    /*@PostMapping
+    public ResponseEntity<ConcorrenteDTO> inserir
+            (@RequestBody ConcorrenteForm CF,
+             UriComponentsBuilder UB){
+
+        Concorrente concorrente = CF.criaConcorrente();
+        concorrenteRepository.save(concorrente);
+        ConcorrenteDTO concorrenteDTO = new ConcorrenteDTO(concorrente);
+        UB.path("/concorrentes/{id}");
+        URI uri = UB.buildAndExpand(concorrente.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(concorrenteDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateConcorrente(@PathVariable Integer id,
+                                               @RequestBody ConcorrenteForm CF){
+
+        if (id == null){
+
+            return ResponseEntity.badRequest().build();
+
+        }
+
+        try{
+
+            Concorrente concorrente = concorrenteRepository.getReferenceById(id);
+            concorrente.setNome(CF.getNome());
+            concorrente.setCpf(CF.getCpf());
+            concorrente.setListaLances(CF.getListaLances());
+            concorrenteRepository.save(concorrente);
+            ConcorrenteDTO concorrenteDTO = new ConcorrenteDTO(concorrente);
+
+            return ResponseEntity.ok(concorrenteDTO);
+
+        } catch (Exception e){
+
+            return ResponseEntity.notFound().build();
+
+        }
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletaConcorrente(@PathVariable Integer id){
+
+        if (id == null){
+
+            return ResponseEntity.badRequest().build();
+
+        }
+
+        try {
+
+            Concorrente concorrente = concorrenteRepository.getReferenceById(id);
+            ConcorrenteDTO concorrenteDTO = new ConcorrenteDTO(concorrente);
+            concorrenteRepository.delete(concorrente);
+
+
+            return ResponseEntity.ok(concorrenteDTO);
+
+        }catch (Exception e){
+
+            return ResponseEntity.notFound().build();
+
+        }*/
+
+    }
+
+
