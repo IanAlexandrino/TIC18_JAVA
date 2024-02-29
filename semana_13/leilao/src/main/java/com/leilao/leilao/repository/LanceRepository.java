266f7acc3e91f9452a3846f2549f9977180d67bb
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-02-29 16:46:50 -0300

    Criando interface do repositório para classe Lance

---
+++ semana_13/leilao/src/main/java/com/leilao/leilao/repository/LanceRepository.java
@@ -0,0 +1,7 @@
+package com.leilao.leilao.repository;
+
+import com.leilao.leilao.model.Lance;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LanceRepository extends JpaRepository<Lance, Integer> {
+}
